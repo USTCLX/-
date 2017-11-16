370c2b7fb1b8c1c743dfb0be98a52128d5b37e48
Xiang Li <348816986@qq.com>
2017-11-16 16:09:27 +0800

    Create example.js
    创建例子

---
+++ example/example.js
@@ -0,0 +1,31 @@
+var scope = new Scope({
+	reciprocal:true
+});
+
+scope.user = {
+	name:'lixiang',
+    age: 25,
+    gender: 2
+};
+
+scope.user.incAge = function() {
+    scope.user.age += 1;
+    scope.digest();
+}
+
+scope.user.decAge = function() {
+    scope.user.age -= 1;
+    scope.digest();
+}
+
+scope.setup();
+
+document.getElementById('switch').addEventListener('click',function(){
+	console.log('haha');
+	scope.user = {
+		name:'xin',
+		age:20,
+		gender:2
+	}
+	// scope.digest()
+},false)
